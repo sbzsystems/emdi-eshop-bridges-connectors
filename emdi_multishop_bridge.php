044a517735f6d9b59eb21cd09d1abcca750dce74
SBZ systems <support@sbzsystems.com>
2020-11-24 21:09:18 +0200

    Add files via upload

--- emdi_multishop_bridge.php
+++ emdi_multishop_bridge.php
@@ -1,17 +1,18 @@
 <?php
 /*------------------------------------------------------------------------
-		# EMDI - MERGE by SBZ systems - Solon Zenetzis - version 1
-		# ------------------------------------------------------------------------
-		# author    SBZ systems - Solon Zenetzis
-		# copyright Copyright (C) 2020 sbzsystems.com. All Rights Reserved.
-		# @license - https://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
-		# Websites: https://www.sbzsystems.com
-		# Technical Support:  Forum - https://www.sbzsystems.com
-	-------------------------------------------------------------------------*/
+# EMDI - MULTISHOP BRIDGE by SBZ systems - Solon Zenetzis - version 1
+# ------------------------------------------------------------------------
+# author    SBZ systems - Solon Zenetzis
+# copyright Copyright (C) 2020 sbzsystems.com. All Rights Reserved.
+# @license - https://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: https://www.sbzsystems.com
+# Technical Support:  Forum - https://www.sbzsystems.com
+-------------------------------------------------------------------------*/
+
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header('Content-Type: text/html; charset=UTF-8');
 error_reporting(0);
- 
+
 //$customerid=$_REQUEST['customerid'];
 $productid=$_REQUEST['productid'];
 $productid=iconv("ISO-8859-7", "UTF-8",  $productid);
@@ -20,97 +21,99 @@ $action=$_REQUEST['action'];       // PRODUCT CODE
 $orderid=$_REQUEST['orderid'];       // PRODUCT CODE
 $key=$_REQUEST['key'];       // PRODUCT CODE
 if (!($key==$passkey)) { exit; }
- 
+
+
+$randomvar='&rndval='.rand(10000000,90000000); //reduce cache issues
 
 if ($action == 'deletetmp') {
- 
-	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=deletetmp");	
-	echo file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=deletetmp");	
+
+	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=deletetmp$randomvar");	
+	echo file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=deletetmp$randomvar");	
 	
 }
 
 
 if ($action == 'customersok') {
 
-	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=customersok");	
-	echo file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=customersok");	
- 
+	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=customersok$randomvar");	
+	echo file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=customersok$randomvar");	
+
 }
 
 
 if ($action == 'productsok') {
-  	
-	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=productsok");	
-	echo file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=productsok");	
+	
+	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=productsok$randomvar");	
+	echo file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=productsok$randomvar");	
 
 }
 
 
 if ($action == 'customers') {
 
-	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=customers");	
+	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=customers$randomvar");	
 	//Delete 1st row
-	echo preg_replace('/^.+\n/', '', file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=customers"));
-		
+	echo preg_replace('/^.+\n/', '', file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=customers$randomvar"));
+	
 }
 
 
 if ($action == 'products') { 
 
-	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=products");	
+	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=products$randomvar");	
 	//Delete 1st row
-	echo preg_replace('/^.+\n/', '', file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=products"));
+	echo preg_replace('/^.+\n/', '', file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=products$randomvar"));
 	
 }
 
 
 if ($action == 'orders') {
-		
-	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=orders");	
+	
+	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=orders$randomvar");	
 	//Delete 1st row
-	echo preg_replace('/^.+\n/', '', file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=orders"));
-		 
+	echo preg_replace('/^.+\n/', '', file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=orders$randomvar"));
+	
 }
 
 
 if ($action == 'order') {
- 
-	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=order&orderid=$orderid");	
+
+	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=order&orderid=$orderid$randomvar");	
 	//Delete 1st row
-	echo preg_replace('/^.+\n/', '', file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=order&orderid=$orderid"));	
+	echo preg_replace('/^.+\n/', '', file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=order&orderid=$orderid$randomvar"));	
 	
 }
 
 
 if ($action == 'confirmorder') {
- 
-	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=confirmorder&orderid=$orderid");	
+
+	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=confirmorder&orderid=$orderid$randomvar");	
 	//Delete 1st row
-	echo preg_replace('/^.+\n/', '', file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=confirmorder&orderid=$orderid"));
+	echo preg_replace('/^.+\n/', '', file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=confirmorder&orderid=$orderid$randomvar"));
 
 }
 
 
 if ($action == 'updatestock') { 
- 
-	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=updatestock&productid=$productid&stock=$stock");	
+
+	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=updatestock&productid=$productid&stock=$stock$randomvar");	
 	//Delete 1st row
-	echo preg_replace('/^.+\n/', '', file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=updatestock&productid=$productid&stock=$stock"));
+	echo preg_replace('/^.+\n/', '', file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=updatestock&productid=$productid&stock=$stock$randomvar"));
 	
 }
 
 
 if ($action == 'cancelorder') {
 	
- 	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=cancelorder&orderid=$orderid");	
+	echo file_get_contents("http://eshop1.gr/emdi_wp_woo_bridge.php?action=cancelorder&orderid=$orderid$randomvar");	
 	//Delete 1st row
-	echo preg_replace('/^.+\n/', '', file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=cancelorder&orderid=$orderid"));
+	echo preg_replace('/^.+\n/', '', file_get_contents("https://eshop2.com/emdi_open2_bridge.php?action=cancelorder&orderid=$orderid$randomvar"));
 	
 } 
 
 
 
- 
+
 
 function base_enc($encoded) {
 	$result='';
