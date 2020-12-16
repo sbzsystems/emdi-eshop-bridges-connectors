063748950cd7a02b58b3d1f790782030766a7c29
SBZ systems <support@sbzsystems.com>
2020-12-16 18:25:30 +0200

    Add files via upload

--- emdi_whmcs_bridge.php
+++ emdi_whmcs_bridge.php
@@ -1,9 +1,9 @@
 ﻿<?php
 /*------------------------------------------------------------------------
-		# EMDI - WHMCS 7 BRIDGE by SBZ systems - Solon Zenetzis - version 1.2
+		# EMDI - WHMCS BRIDGE by SBZ systems - Solon Zenetzis - version 1.2
 		# ------------------------------------------------------------------------
 		# author    SBZ systems - Solon Zenetzis
-		# copyright Copyright (C) 2013-2017 sbzsystems.com. All Rights Reserved.
+		# copyright Copyright (C) 2013-2020 sbzsystems.com. All Rights Reserved.
 		# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
 		# Websites: http://www.sbzsystems.com
 		# Technical Support:  Forum - http://www.sbzsystems.com
@@ -14,12 +14,10 @@ header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 
 
 require "configuration.php";
-
-
 $maintax=24;
 // Connects to your Database
-mysql_connect("$db_host", $db_username, $db_password) or die(mysql_error());
-mysql_select_db("$db_name") or die(mysql_error());
+$link=mysqli_connect("$db_host", $db_username, $db_password) or die(mysqli_error($link));
+mysqli_select_db($link,"$db_name") or die(mysqli_error($link));
 
 $ip=$_SERVER['REMOTE_ADDR'];   // USER'S IP 
 $action=$_REQUEST['action'];       // PRODUCT CODE
@@ -29,13 +27,12 @@ $tmp_path=realpath(dirname(__FILE__)).'/templates_c';
 $measure='ΤΕΜΑΧΙΟ';
 $customerid=$_REQUEST['customerid'];
 $productid=$_REQUEST['productid'];
+$test=$_REQUEST['test'];
 $relatedchar='^';
 
 
-if (!($key=='dfhtj56hfrgw1')) { exit; }
+//if (!($key=='')) { exit; }
 
-
-//echo $tmp_path;
 
 
 
@@ -49,16 +46,47 @@ if ($action == 'deletetmp') {
 if ($action == 'customersok') {
 	$File = $tmp_path."/customers_".$key; 
 	$Handle = fopen($File, 'w');
-	$Data = time(); 
-	fwrite($Handle, $Data); 
-	fclose($Handle); 	
+	//$Data = time(); 
+	//fwrite($Handle, $Data); 
+	//fclose($Handle); 	
+	
+	
+	//GET SQL TIME
+    $data = mysqli_query($link, "SELECT NOW() dtime") or die(mysqli_error($link));
+    while ($alldata = mysqli_fetch_array($data)) {
+        $dtime = $alldata['dtime'];
+        break;
+    }
+    //
+    fwrite($Handle, $dtime);
+    fclose($Handle);
+    
+    mysqli_close($link);
+	
+	
 }
 if ($action == 'productsok') {
 	$file = $tmp_path."/products_".$key; 
-	$handle = fopen($file, 'w');
-	$data = time();
-	fwrite($handle, $data); 
-	fclose($handle); 	
+	$Handle = fopen($file, 'w');
+	//$data = time();
+	//fwrite($handle, $data); 
+	//fclose($handle); 	
+	
+	
+	
+	//GET SQL TIME
+    $data = mysqli_query($link, "SELECT NOW() dtime") or die(mysqli_error($link));
+    while ($alldata = mysqli_fetch_array($data)) {
+        $dtime = $alldata['dtime'];
+        break;
+    }
+    //
+    fwrite($Handle, $dtime);
+    fclose($Handle);
+    
+    mysqli_close($link);
+	
+	
 }
 
 
@@ -80,11 +108,11 @@ if ($action == 'productsok') {
 
 //// GET PRODUCT ID 
 //$query = "SET NAMES 'utf-8'";
-//mysql_query($query);  
+//mysqli_query($link,$query);  
 
 
 //$query = "SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'";
-//mysql_query($query);  
+//mysqli_query($link,$query);  
 
 
 
@@ -94,18 +122,17 @@ if ($action == 'customers') {
 	$lastdate=0;
 	if (file_exists($file)) {
 		$handle = fopen($file, 'r'); 
-		$lastdate = fread($handle, 11); 
+		$lastdate = fread($handle, 20); 
 		fclose($handle); 
 	}
 	
 	
 	
 	
-	$data = mysql_query(" SELECT * FROM tblclients where updated_at>FROM_UNIXTIME(".$lastdate.")") or die(mysql_error()); 
-	echo "CUSTOMER ID;FIRST NAME;LAST NAME;ADDRESS;ZIP;COUNTRY;CITY/STATE;AREA;PHONE;MOBILE;EMAIL;VAT;TAX OFFICE;COMPANY;OCCUPATION;LANGUAGE;PO BOX;<br>\n";
-
+	$data = mysqli_query($link," SELECT * FROM tblclients where '$test'='1' or lastdate>'" . $lastdate . "'") or die(mysqli_error($link)); 
+	echo "ΚΩΔΙΚΟΣ;ΟΝΟΜΑ;ΕΠΙΘΕΤΟ;ΔΙΕΥΘΥΝΣΗ;ΤΚ;ΧΩΡΑ;ΠΟΛΗ/ΝΟΜΟΣ;ΠΕΡΙΟΧΗ;ΤΗΛΕΦΩΝΟ;ΚΙΝΗΤΟ;EMAIL;ΑΦΜ;ΔΟΥ;ΕΠΩΝΥΜΙΑ;ΕΠΑΓΓΕΛΜΑ;ΓΛΩΣΣΑ;<br>\n";
 	
-	while($alldata = mysql_fetch_array( $data ))
+	while($alldata = mysqli_fetch_array( $data ))
 	{
 		
 		$alldata=str_replace('&amp;', 'k', $alldata);
@@ -130,15 +157,15 @@ if ($action == 'customers') {
 		//$notes=iconv('UTF-8','ISO-8859-7',  $alldata['notes']);  	 	
 		
 		
-		$datain = mysql_query('SELECT * FROM tblcustomfieldsvalues where tblcustomfieldsvalues.relid='.$id.'
-			ORDER BY tblcustomfieldsvalues.relid,tblcustomfieldsvalues.fieldid ASC') or die(mysql_error()); 
+		$datain = mysqli_query($link,'SELECT * FROM tblcustomfieldsvalues where tblcustomfieldsvalues.relid='.$id.'
+			ORDER BY tblcustomfieldsvalues.relid,tblcustomfieldsvalues.fieldid ASC') or die(mysqli_error($link)); 
 		
-		while($alldatain = mysql_fetch_array( $datain ))
+		while($alldatain = mysqli_fetch_array( $datain ))
 		{			
 			$field_[$alldatain['fieldid']]=$alldatain['value']; 
 		}
 		
-		$afm=$field_[2];
+		$afm=$alldata['tax_id'];
 		$doy=$field_[3];
 		$mobile=$field_[4];
 		$epaggelma=$field_[5];
@@ -148,6 +175,7 @@ if ($action == 'customers') {
 		echo 'C'.$id.';'.$firstname.';'.$lastname.';'.$address1.';'.$postcode.';'.$country.';'.$state.';'.$city.';'
 		.$phonenumber.';'.$mobile.';'.$email.';'.$afm.';'.$doy.';'.$companyname.';'.$epaggelma.';'.$language,";<br>\n";
 	}
+
 }
 
 
@@ -171,7 +199,7 @@ if ($action == 'products') {
 	$lastdate=0;
 	if (file_exists($file)) {
 		$handle = fopen($file, 'r'); 
-		$lastdate = fread($handle, 11); 
+		$lastdate = fread($handle, 20); 
 		fclose($handle); 
 	}
 	
@@ -182,7 +210,7 @@ if ($action == 'products') {
 	
 	
 	////PRODUCTS
-	$data = mysql_query("SELECT tblproducts.type as keym, tblproducts.paytype, tblpricing.type,tblproducts.id,tblproducts.name as name1,tbltax.taxrate,tblproducts.servertype,
+	$data = mysqli_query($link,"SELECT tblproducts.type as keym, tblproducts.paytype, tblpricing.type,tblproducts.id,tblproducts.name as name1,tbltax.taxrate,tblproducts.servertype,
 		
 		tblpricing.monthly,  tblpricing.quarterly, tblpricing.semiannually, tblpricing.annually, tblpricing.biennially, tblpricing.triennially, tblpricing.asetupfee,
 		tblproductgroups.name as name2
@@ -198,14 +226,14 @@ if ($action == 'products') {
 		on tblproductgroups.id=tblproducts.gid
 		
 		where
-		tblproducts.updated_at>FROM_UNIXTIME(".$lastdate.")
+		tblproducts.lastdate>'" . $lastdate . "'
 		
 		
 		group by name1
-		") or die(mysql_error()); 
-	echo "PRODUCT ID;DESCRIPTION1;DESCRIPTION2;TAX;PRICE;PURCHASE PRICE;AVAILABILITY;MEASUREMENT UNIT;CATEGORY;PHOTO;URL;CATEGORY ORDER;<br>\n";
+		") or die(mysqli_error($link)); 
+	echo "ΚΩΔΙΚΟΣ;ΠΕΡΙΓΡΑΦΗ1;ΠΕΡΙΓΡΑΦΗ2;ΦΠΑ;ΤΙΜΗ1;ΤΙΜΗ2;ΔΙΑΘΕΣΙΜΟΤΗΤΑ;ΜΟΝΑΔΑ;ΚΑΤΗΓΟΡΙΑ<br>\n";
 	
-	while($alldata = mysql_fetch_array( $data ))
+	while($alldata = mysqli_fetch_array( $data ))
 	{
 		$paytype=substr($alldata['paytype'],0,1);
 		$type=substr($alldata['keym'],0,1);
@@ -236,18 +264,18 @@ if ($action == 'products') {
 		
 	}
 	////////DOMAINS
-	$data = mysql_query("SELECT * FROM tbldomainpricing
+	$data = mysqli_query($link,"SELECT * FROM tbldomainpricing
 		
 		left join tblpricing
 		on tblpricing.relid=tbldomainpricing.id and tblpricing.type like 'domain%'
 		
 		where
-		tblpricing.lastdate>FROM_UNIXTIME(".$lastdate.")
+		tblpricing.lastdate>'" . $lastdate . "'
+		group by type
 		
-		
-		") or die(mysql_error()); 
+		") or die(mysqli_error($link)); 
 	
-	while($alldata = mysql_fetch_array( $data ))
+	while($alldata = mysqli_fetch_array( $data ))
 	{
 		$id=$alldata['id'];  	 	
 		$name= $alldata['extension']; 
@@ -312,23 +340,91 @@ if ($action == 'products') {
 
 
 if ($action == 'orders') {
-	$data = mysql_query("SELECT * FROM tblinvoices where notes=''  order by userid,id desc") or die(mysql_error()); //and tax<>0
-	echo "ORDER ID;CUSTOMER ID;SHIPPING COST;PAYMENT COST;DISCOUNT;DATE;NOTE;USER;VOUCHER;<br>\n";
+	
+	$query="
+	SELECT * 
+	
+	,
+	case when status='Paid' then
+	(SELECT group_concat(concat(acc.gateway,' ',DATE_FORMAT(acc.date,'%d/%m/%Y'),' ',acc.amountin)) FROM tblaccounts acc where acc.invoiceid=tblinvoices.id) 
+	else '' end
+	bank
 	
 	
-	while($alldata = mysql_fetch_array( $data ))
+	,
+	case when status='Paid' then
+	(SELECT acc.gateway FROM tblaccounts acc where acc.invoiceid=tblinvoices.id limit 1) 
+	else '' end
+	payment
+	,
+	
+	
+	
+    (SELECT GROUP_CONCAT(	tblinvoiceitems.description) FROM tblinvoiceitems where tblinvoiceitems.invoiceid=tblinvoices.id) description
+	
+	
+	
+	
+	
+	FROM tblinvoices 
+	where status<>'Cancelled' 
+	and notes='' 
+	
+	and (tax<>0 or (SELECT tblclients.taxexempt FROM tblclients where tblclients.id=tblinvoices.userid)=1) 
+	
+	and (SELECT tblclients.status FROM tblclients where tblclients.id=tblinvoices.userid)='Active'
+	
+	
+	order by userid,id desc
+	";
+	
+	$data = mysqli_query($link,$query) or die(mysqli_error($link)); //
+	echo "ΚΩΔΙΚΟΣ ΠΑΡΑΓΓΕΛΙΑΣ;ΚΩΔΙΚΟΣ ΠΕΛΑΤΗ;ΚΟΣΤΟΣ ΜΕΤΑΦΟΡΙΚΩΝ;ΚΟΣΤΟΣ ΑΝΤΙΚΑΤΑΒΟΛΗΣ;ΕΚΠΤΩΣΗ;ΗΜΕΡΟΜΗΝΙΑ;ΣΧΟΛΙΟ;<br>\n";
+	
+	while($alldata = mysqli_fetch_array( $data ))
 	{
 		$id=$alldata['id'];  	 	
 		$userid= $alldata['userid']; 
 		$docdate= strtotime(  $alldata['date']); 
 		$docdate=date ("Y-m-d H:i:s", $docdate);
-		$notes= $alldata['paymentmethod']; 
+		$notes= $alldata['bank'];
+		$payment= $alldata['payment'];
+		$description= $alldata['description'];
+		
+		if ($payment=='banktransfer_alpha') { $payment='ΚΑΤΑΘΕΣΗ - ALPHA'; }
+		if ($payment=='banktransfer_nbg') { $payment='ΚΑΤΑΘΕΣΗ - ΕΘΝΙΚΗ'; }
+		if ($payment=='banktransfer_piraeus') { $payment='ΚΑΤΑΘΕΣΗ - ΠΕΙΡΑΙΩΣ'; }
+		if ($payment=='banktransfer_eurobank') { $payment='ΚΑΤΑΘΕΣΗ - EUROBANK'; }
+		if ($payment=='banktransfer_viva') { $payment='ΚΑΤΑΘΕΣΗ - VIVA WALLET'; }
+		if ($payment=='banktransfer_revolut') { $payment='ΚΑΤΑΘΕΣΗ - REVOLUT'; }
+		if ($payment=='stripe') { $payment='ΚΑΡΤΑ - STRIPE'; }
+		if ($payment=='stripe_prebuilt1') { $payment='ΚΑΡΤΑ - STRIPE'; }
+		if ($payment=='vivapayments') { $payment='ΚΑΡΤΑ - VIVA WALLET'; }
+		if ($payment=='ethniki') { $payment='ΚΑΡΤΑ - ΕΘΝΙΚΗ'; }
+
+		
+		
+		
+	if ( stripos($description,'host') || 
+	stripos($description,'dedicated') || 
+	stripos($description,'domain') || 
+	stripos($description,'.gr') ||
+	stripos($description,'.com') ||
+	stripos($description,'ΤΗΛΕΦΩΝΙΚΟ') ||
+		stripos($description,'PLUGIN') )	  
+		{
+			$document='ΤΙΜΟΛΟΓΙΟ ΠΑΡΟΧΗΣ ΥΠΗΡΕΣΙΩΝ';
+		} else {
+			$document='ΤΙΜΟΛΟΓΙΟ ΠΑΡΟΧΗΣ ΛΟΓΙΣΜΙΚΟ';
+		}
+		
+		
+	 
 		
 		
 		
 		
-		
-		echo $id.';C'.$userid.";0;0;0;$docdate;$notes;<br>\n";
+		echo $id.';C'.$userid.";0;0;0;$docdate;$notes;;;;;$payment;ONLINE;$document;<br>\n";
 		
 	}
 }
@@ -361,7 +457,7 @@ if ($action == 'orders') {
 if ($action == 'order') {
 	
 	////HOSTING
-	$data = mysql_query("SELECT 
+	$data = mysqli_query($link,"SELECT 
 		tblproducts.type as type1, tblhosting.billingcycle,
 		tblhosting.packageid,
 		tblinvoiceitems.description, tblinvoiceitems.amount, tblinvoiceitems.type
@@ -376,10 +472,10 @@ if ($action == 'order') {
 		on tblproducts.id=tblhosting.packageid
 		
 		
-		where invoiceid=".$orderid) or die(mysql_error()); 
-	echo "PRODUCT ID;DESCRIPTION 1;DESCRIPTION 2;DESCRIPTION 3;QUANTITY;MEASUREMENT UNIT;PRICE;TAX;DISCOUNT;START DATE;END DATE;POSITION;ORDER ID;<br>\n";
+		where invoiceid=".$orderid) or die(mysqli_error($link)); 
+	echo "ΚΩΔΙΚΟΣ;ΠΕΡΙΓΡΑΦΗ1;ΠΕΡΙΓΡΑΦΗ2;ΠΕΡΙΓΡΑΦΗ3;ΠΟΣΟΤΗΤΑ;ΜΟΝΑΔΑ;ΤΙΜΗ;ΦΠΑ;ΕΚΠΤΩΣΗ;<br>\n";
 	
-	while($alldata = mysql_fetch_array( $data ))
+	while($alldata = mysqli_fetch_array( $data ))
 	{
 		
 		$type=$alldata['type'];
@@ -472,11 +568,20 @@ if ($action == 'order') {
 if ($action == 'confirmorder') {
 	//$hmera=gmdate('d/m/Y H:i:s');
 	
+
+	if ($_REQUEST['docid']==95) {
+		echo $hmera;
+		exit;
+	}
+	
+
+	
 	$timezone=2; 
 	$hmera=gmdate("d/m/Y H:i:s", time() + 3600*($timezone+date("I"))); 
 	
+		
+	$data = mysqli_query($link,"UPDATE tblinvoices SET notes = 'ΚΟΠΗΚΕ ΤΙΜΟΛΟΓΙΟ ".$hmera."' WHERE notes='' and tblinvoices.id in (".$orderid.")") or die(mysqli_error($link)); 
 	
-	$data = mysql_query("UPDATE tblinvoices SET notes = 'ΚΟΠΗΚΕ ΤΙΜΟΛΟΓΙΟ ".$hmera."' WHERE notes='' and tblinvoices.id in (".$orderid.")") or die(mysql_error()); 
 	
 	echo $hmera;
 	
@@ -489,7 +594,7 @@ if ($action == 'cancelorder') {
 	$hmera=gmdate("d/m/Y H:i:s", time() + 3600*($timezone+date("I"))); 
 	
 	
-	$data = mysql_query("UPDATE tblinvoices SET notes = '-' WHERE notes='' and tblinvoices.id in (".$orderid.")") or die(mysql_error()); 
+	$data = mysqli_query($link,"UPDATE tblinvoices SET notes = '-' WHERE notes='' and tblinvoices.id in (".$orderid.")") or die(mysqli_error($link)); 
 	
 	echo $hmera;
 	
@@ -563,4 +668,4 @@ if ($action == 'redirect') {
 
 
 
-?> 		
+?>
